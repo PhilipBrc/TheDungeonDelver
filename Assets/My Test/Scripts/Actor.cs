f77f6b636bcc75a7ae07d0dd6a4629be979cdf98
PhilipB <goalloutcreative@gmail.com>
2019-11-04 00:10:16 -0500

    stuff with Inkle and actors

---
+++ Assets/My Test/Scripts/Actor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    public Sprite[] emotionSprites;
+    SpriteRenderer spRend;
+    public int ID;
+    public enum CharacterEmotion
+    {
+        neutral, happy, sad, angry
+    }
+    [SerializeField]
+    CharacterEmotion myState;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myState = CharacterEmotion.neutral;
+        spRend = GetComponent<SpriteRenderer>();
+        
+    }
+    public void ChangeState(string emotionName)
+    {
+        StartCoroutine(emotionName + "State");
+    }
+    IEnumerator NeutralState()
+    {
+        spRend.sprite = emotionSprites[0];
+        myState = CharacterEmotion.neutral;
+        yield return null;
+    }
+    IEnumerator HappyState()
+    {
+        spRend.sprite = emotionSprites[1];
+            myState = CharacterEmotion.happy;
+        yield return null;
+    }
+
+    IEnumerator SadState()
+    {
+        spRend.sprite = emotionSprites[2];
+        myState = CharacterEmotion.sad;
+        yield return null;
+    }
+
+    IEnumerator AngryState()
+    {
+        spRend.sprite = emotionSprites[3];
+        myState = CharacterEmotion.angry;
+        yield return null;
+    }
+
+
+}
---
+++ Assets/My Test/Scripts/CharacterManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject[] characters;
+    public List<GameObject> actorsList = new List<GameObject>();
+    [SerializeField]
+    Vector3 leftActorPosition, rightActorPosition;
+
+    List<Actor> activeActors = new List<Actor>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < characters.Length; i++)
+        {
+            GameObject newActor = Instantiate(characters[i]);
+            newActor.SetActive(false);
+            newActor.name = characters[i].name;
+            actorsList.Add(newActor);
+        //newActor.GetComponent<Actor>().ID = 
+        }
+    }
+    public void PlaceActors(string leftActorName, string rightActorName)
+    {
+
+        foreach (GameObject g0 in actorsList)
+        {
+            if(g0.name == leftActorName)
+            {
+                g0.SetActive(true);
+                g0.GetComponent<Actor>().ID = 0;
+                activeActors.Add(g0.GetComponent<Actor>());
+                g0.transform.position = leftActorPosition;
+            }else if (g0.name == rightActorName)
+            {
+                g0.SetActive(true);
+                g0.GetComponent<Actor>().ID = 1;
+                activeActors.Add(g0.GetComponent<Actor>());
+                g0.transform.position = rightActorPosition;
+            }
+
+        }
+    }
+
+    public void ChangeActorEmotion(string emotion, int ID)
+    {
+        foreach (Actor actor in activeActors)
+        {
+            if (actor.gameObject.activeInHierarchy)
+            {
+                if (actor.ID == ID)
+                {
+                    actor.ChangeState(emotion);
+                }
+            }
+        }
+    }
+}
